ebe0c0559096f0b7e5fa8b17da8504e8583d5763
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-04 15:13:49 +0530

    Update variable descriptions in variable.tf

--- aws/variable.tf
+++ aws/variable.tf
@@ -1,12 +1,14 @@
 variable "ami_value" {
-  description = "value of the ami "
+  description = "The AMI ID for the EC2 instance"
+  type        = string
 }
 
 variable "instance_type_value" {
-  description = "the type like t2.miceo or large"
-
+  description = "The size of the instance (e.g., t2.micro)"
+  type        = string
 }
 
-variable "subnet_id_value"  {
-   description = "the id of subnet"
+variable "subnet_id_value" {
+  description = "The VPC subnet ID"
+  type        = string
 }
