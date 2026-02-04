d0494fedd23753d03f56e18e03c81c0e970b712f
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-04 14:18:28 +0530

    Add variables for AMI, instance type, and subnet ID

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+  description = "value of the ami "
+}
+
+variable "instance_type_value" {
+  description = "the type like t2.miceo or large"
+
+}
+
+variable "subnet_id_value"  {
+   description = "the id of subnet"
+}
